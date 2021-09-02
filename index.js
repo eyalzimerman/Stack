1fe56d84fc2103441135c9fddcb122dcff326a3e
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-09-02 11:01:34 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+// [1]  get min => 1   newStack = [1]
+// [1,2]  get min => 1 newStack = [1]
+// [1,2,3]  get min => 1 newStack = [1]
+// [1,2,3,0]  get min => 0  newStack = [1,0]
+// [1,2,3,0,10]  get min => 0 newStack = [1,0]
+// [1,2,3,0,10,-1]  get min => -1  newStack = [1,0,-1]
+// [1,2,3,0,10,]  get min => 0  newStack = [1,0]
+// [1,2,3]  get min => 1 newStack = [1]
+
+// [1,2,3]  get min => 1
+
+class Stack {
+  constructor() {
+    this.mainStack = [];
+    this.minSaverStack = [];
+  }
+
+  getMin = () => {
+    if (this.minSaverStack.length > 0) {
+      const min = this.minSaverStack.pop();
+      this.minSaverStack.push(min);
+      return min;
+    } else {
+      return null;
+    }
+  };
+
+  push = (number) => {
+    const arrLength = this.mainStack.length;
+
+    this.mainStack[arrLength] = number;
+  };
+}
